81a2a58677c4558cdcadf6ff901a6fe10690ea90
SeaweedOne <rlakskchlrh@naver.com>
2021-05-31 09:56:55 +0900

    .

--- BaekJoon2294DP_Coin_MinCase.java
+++ BaekJoon2294DP_Coin_MinCase.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+﻿import java.util.Scanner;
 
 public class BaekJoon2294DP_Coin_MinCase {
 	public static void main(String[] args) {
@@ -15,7 +15,7 @@ public class BaekJoon2294DP_Coin_MinCase {
 		}
 
 		for (int i = 1; i <= k; i++) { // 만들 수 있는지 없는지 보기 위해서 최대값인 100000보다 1큰 수를 초기 수로 지정
-			dp[i] = 100001;
+			dp[i] = 999999;
 		}
 
 		for (int i = 0; i < n; i++) { // 코인 가져올 배열
@@ -30,7 +30,7 @@ public class BaekJoon2294DP_Coin_MinCase {
 		}
 
 		int result = dp[k]; // k원에서 만들 수 있는 최소값을 결과 변수에 저장
-		if (result == 100001) { // dp[k] 값이 100001이라면? 우리가 초기화 해둔 값이라면? 최소값이 없다는 소리 , 그렇다면 -1 출력
+		if (result == 999999) { // dp[k] 값이 100001이라면? 우리가 초기화 해둔 값이라면? 최소값이 없다는 소리 , 그렇다면 -1 출력
 			System.out.println(-1);
 		} else { // 최소값이 저장되어있는 경우 최소값 출력
 			System.out.println(dp[k]);
